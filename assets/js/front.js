87203be253ec81e36c1f5eb88039495bd0df31ec
mrduhart <9971552+mrduhart@users.noreply.github.com>
2020-05-08 19:47:34 -0500

    convert to jekyll

--- assets/js/front.js
+++ assets/js/front.js
@@ -87,7 +87,7 @@ function map() {
     if ($('#' + mapId).length > 0) {
 
         var icon = L.icon({
-            iconUrl: 'img/marker.png',
+            iconUrl: 'assets/img/marker.png',
             iconSize: [25, 37.5],
             popupAnchor: [0, -18],
             tooltipAnchor: [0, 19]
